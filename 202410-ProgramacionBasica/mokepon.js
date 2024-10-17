b75dcadb30646584707fda201a7d80945cfb77fd
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-10-16 21:47:03 -0600

    carga de html, css y java script para el manejo de canva

--- 202410-ProgramacionBasica/mokepon.js
+++ 202410-ProgramacionBasica/mokepon.js
@@ -1,3 +1,29 @@
 const sectionSeleccionarAtaque = document.getElementById
 ('seleccionar-ataque')
 const sectionReiniciar = document.getElementById('reiniciar')
+const botonMascotaJugador = document.getElementById('boton-mascota')
+const botonReiniciar = document.getElementById('boton-reiniciar')
+sectionReiniciar.style.display = 'none'
+
+ const sectionSeleccionarMascota = document.getElementById
+ ('seleccionar-mascota')
+ const spanMascotaJugador = document.getElementById('mascota-jugador')
+
+ const spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+ const spanVidasJugador = document.getElementById('vidas-jugador')
+ const spanVidasEnemigo = document.getElementById('vidas-enemigo')
+ 
+ const sectionMensajes = docoment.getElementById('resultado') 
+
+
+
+
+ class Mokepon {
+    constructor(nombre, foto, vida) {
+        this.nombre = nombre
+        this.foto = foto
+        this.vida = vida
+        this.ataques = []
+    }
+ }
