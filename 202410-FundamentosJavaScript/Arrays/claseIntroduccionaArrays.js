f1c4602b6cf5e9be0e398920ffb3c1ac892951ad
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-10-24 20:00:41 -0600

    Inicio de curso de javascript

---
+++ 202410-FundamentosJavaScript/Arrays/claseIntroduccionaArrays.js
@@ -0,0 +1,15 @@
+//How to create an array
+
+//1. new Array or Array()
+
+const fruits = Array('Apple', 'Banana','Orange')
+console.log(fruits)
+
+const recibeIngredientes = [
+    'Flour',
+    true,
+    2,
+    {
+        ingredient: 'Milk', quantity: 1
+    }
+]
