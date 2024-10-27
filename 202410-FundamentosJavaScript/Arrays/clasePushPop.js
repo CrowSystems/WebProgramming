6ecccddf59ea29b619d8d07a5759f941df9e9bbc
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-10-26 21:02:15 -0600

    Clase: Como manejar los Array con Push y Pop

---
+++ 202410-FundamentosJavaScript/Arrays/clasePushPop.js
@@ -0,0 +1,17 @@
+//Methods that modify the original array (Mutability)
+
+//Push()
+
+const countries = ['USA', 'Canada', 'UK']
+const newCountries = countries.push('Germany', 'Australia')
+
+console.log(countries)
+console.log(newCountries)
+
+
+//Pop()
+
+const removeCountry = countries.pop()
+
+console.log(countries)
+console.log(removeCountry)
