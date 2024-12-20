f287394f2bd2c29a1ab4c6238d6bf0ed0f3f787e
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-20 17:28:12 -0600

    Manejo de paginación y manejo de endpoints

--- Backend con NodeJs/Backend_Nodejs_API_REST_Expressjs/my-store/index.js
+++ Backend con NodeJs/Backend_Nodejs_API_REST_Expressjs/my-store/index.js
@@ -6,6 +6,37 @@ app.get('/', (req, res) => {
   res.send('HOLA MI SERVER EN EXPRESS');
 })
 
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Producto 1',
+      price: 1000
+    },
+    {
+      name: 'producto 2',
+      price: 2000
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+  name: 'Product 2',
+  price: 2000
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
 app.listen(port, () =>{
   console.log('Mi port' + port);
 });
