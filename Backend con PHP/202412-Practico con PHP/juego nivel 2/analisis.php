dbaded88e90e17d8b482144c87921ccdfb4224f4
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-20 10:10:51 -0600

    Juego practico nivel 3 con PHP

--- Backend con PHP/202412-Practico con PHP/juego nivel 2/analisis.php
+++ Backend con PHP/202412-Practico con PHP/juego nivel 2/analisis.php
@@ -1,29 +1,10 @@
 <?php
-print_r($_REQUEST);
-
-$palabras = array("sol","luna","cielo");
-
-if($_REQUEST["posicion0"] == $palabras[0]) {
-    echo "<br/>";
-    echo "La palabra ingresada es correcta";
-} else {
-    echo "<br/>";
-    echo "La palabra ingresada es incorrecta, la palabra correcta es: ".$palabras[0];
-}
-
-if($_REQUEST["posicion1"] == $palabras[1]) {
-    echo "<br/>";
-    echo "La palabra ingresada es correcta";
-} else {
-    echo "<br/>";
-    echo "La palabra ingresada es incorrecta, la palabra correcta es: ".$palabras[1];
-}
-
-if($_REQUEST["posicion2"] == $palabras[2]) {
-    echo "<br/>";
-    echo "La palabra ingresada es correcta";
-} else {
-    echo "<br/>";
-    echo "La palabra ingresada es incorrecta, la palabra correcta es: ".$palabras[2];
+$palabras = array("sol","luna","cielo","luz","lluvia");
+for($i=0; $i<count($palabras); $i++){
+    if($_REQUEST["palabra".$i] == $palabras[$i]){
+        echo "La palabra ingresada es correcta"."<br/>";
+    } else {
+        echo "La palabra ingresada es incorrecta, la palabra correcta es: ".$palabras[$i]."<br/>";
+    }
 }
 ?>
