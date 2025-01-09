bb29b99441fb4e3f206a0db1eacc7cfe42833b15
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-08 23:55:25 -0600

    Manejo de strings con JavaScript

--- JAVASCRIPT/202501-javascriptFundamentos/clase-variables.js
+++ JAVASCRIPT/202501-javascriptFundamentos/clase-variables.js
@@ -1,3 +1,21 @@
 let cajaDeAndy = 'Woody'
 
 console.log(cajaDeAndy)
+//Las variables deben de ser descriptivas por buenas practicas
+
+//Lo No permitido en la declaración de variable por buenas practicas.
+let c = 'Woody'
+let scd = 'Woody'
+let pcAndy = 'Woody'
+//estas variables a lo largo no sera posible identificarlas dado al crecimiento del codigo.
+
+//Lo que si se recomienda
+let primerTrasteoDeAndy = 'Woody'
+let urlDelUsuario ='https:/www.google.com' 
+let idDelUsuario = "123456789"
+
+/**
+ * Let:     se utiliza para variables que van a cambiar a lo largo del codigo
+ * const:   es una variable que es una constante fija y no se movera a lo largo del
+ *          programa.
+ */
---
+++ JAVASCRIPT/202501-javascriptFundamentos/string.js
@@ -0,0 +1,9 @@
+let string1 = 'Hola Mundo'
+let string2 = "JavaScript es genial"
+let string3 = `${string2} feliz :)`
+let string4 = string1 + ' ' + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
