1b84540a3eb705574f7879d792ab9db125611ea8
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-10 16:44:22 -0600

    Manejo de datos primitivos y Complejos

---
+++ JAVASCRIPT/202501-fundamentosdeJavaScript/tipoDatos.js
@@ -0,0 +1,34 @@
+//Datos primitivos
+//string
+let nombre = 'Tere'
+
+//number
+let edad = 25
+
+//boolean
+let esMayorDeEdad = true
+
+//null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//symbol
+let simboloUnico = Symbol('unico')
+
+//bigint
+
+
+//********COMPLEJOS**************
+//Object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//Array
+let frutas = ['Manzan', 'banano', 'uvas' ]
+
+//function
+function saludar (){}
