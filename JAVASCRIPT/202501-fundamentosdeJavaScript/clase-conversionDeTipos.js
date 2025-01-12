9463f0476685c866abf829d6596ecdd20de03867
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-12 17:24:45 -0600

    Manejo de estructuras de conversión con javascript

--- JAVASCRIPT/202501-fundamentosdeJavaScript/clase-conversionDeTipos.js
+++ JAVASCRIPT/202501-fundamentosdeJavaScript/clase-conversionDeTipos.js
@@ -5,3 +5,30 @@ console.log(integer)
 console.log(typeof integer)
 
 const stringDecimal = '3.14'
+const float = parseFloat(stringDecimal)
+console.log(float)
+console.log(typeof float)
+
+//Convertir datos de binario a decimal
+const binary = '1010'
+const decimal = parseInt(binary,2)
+console.log(decimal)
+console.log(typeof decimal)
+
+//Implicit type casting
+const sum = '5'+3
+console.log(sum)
+
+const sumwithBoolean = '3' + true
+console.log(sumwithBoolean)
+
+const stringValue = '10'
+const numberValue = 10
+const booleanValue = true
+console.log(stringValue + stringValue)
+console.log(stringValue + numberValue)
+console.log(stringValue + booleanValue)
+console.log(numberValue + stringValue)
+console.log(numberValue + numberValue)
+console.log(numberValue + booleanValue)
+console.log(booleanValue + stringValue)
