b560fc5a9e0fb20343bd8c5a2c3eb54e2908c0be
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-13 12:46:09 -0600

    Manejo de la estructura de Do While con JavaScript

---
+++ JAVASCRIPT/202501-fundamentosdeJavaScript/ciclos/claseDoWhile.js
@@ -0,0 +1,10 @@
+/**
+ * La sentencia Do While crea un bucle que ejecuta una sentencia especificada, hasta que la
+ * condición de comprobación se evalúa como false. La condición se evalúa después de ejecutar
+ * la sentencia, dando como resultado que la sentencia especificada se ejecute al menos una vez.
+ */
+let contador = 0;
+do {
+    console.log(contador);
+    contador++;
+} while(contador<10);
--- JAVASCRIPT/202501-fundamentosdeJavaScript/ciclos/claseWhile.js
+++ JAVASCRIPT/202501-fundamentosdeJavaScript/ciclos/claseWhile.js
@@ -1,7 +1,15 @@
 /**
  * El ciclo While es una instrucción que crea un bucle  que ejecuta una sentencia mientras se cumpla una condición,
  * La condición se evalua antes de ejecutar la sentencia, y si es falsa la ejecuión continúa con la sentencia siguiente.
- * Para ejecutar multiples sentencias dentro de un while 
+ * Para su ejecución es necesario utilizar la sentencia en bloque.
+ * 
+ * n=0;
+ * x=0;
+ * while(n<3)
+ * {
+ *  n++;
+ *  x += n;
+ * }
  */
 
 let contador = 0;
