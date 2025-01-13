aef6472d34c2df22b088486f375b7d59e17ee187
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-12 23:38:23 -0600

    Clase manejo de estructura de for of

---
+++ JAVASCRIPT/202501-fundamentosdeJavaScript/ciclos/claseForOf.js
@@ -0,0 +1,11 @@
+/**
+ * For of, este esta estructura es una variación del
+ * ciclo for, que se utiliza para recorrer todos los
+ * valores de los elementos de un array.
+ */
+
+let canasta = ["manzana","pera","naranja","uva"];
+
+for (fruta of canasta){
+    console.log(fruta);
+}
