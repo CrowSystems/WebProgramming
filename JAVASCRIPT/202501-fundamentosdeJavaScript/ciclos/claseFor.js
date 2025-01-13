eeea4f5ff73b9aa6d1ef79d6a829da5be0ce554d
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-12 23:27:24 -0600

    Clase de manejo de estructura for

---
+++ JAVASCRIPT/202501-fundamentosdeJavaScript/ciclos/claseFor.js
@@ -0,0 +1,13 @@
+/**
+ * El ciclo for es una estructura de programación
+ * que se utiliza para ejecutar un bloque de código
+ * un número específico de veces, es uno de los ciclos
+ * más utilizados en programación y se emplea para 
+ * correr vectores, matrices y estructuras.
+ */
+
+let list = ["eat","sleep","code","repeat"];
+
+for (let i = 0; i < list.length; i++){
+    console.log(list[i]);
+}
