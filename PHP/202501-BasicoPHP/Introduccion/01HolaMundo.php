263f35ccfc3cfde92a0479185646b8482dc7dedd
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-01-31 19:41:30 -0600

    Manejo de variables y combinación de caracteres

--- PHP/202501-BasicoPHP/Introduccion/01HolaMundo.php
+++ PHP/202501-BasicoPHP/Introduccion/01HolaMundo.php
@@ -1,2 +1,8 @@
 <?php
-echo "Hola Mundo";
+echo "Manejo de sintaxis dentro PHP \n";
+
+//Manejo de variables
+$nombre = "Carlos";
+$apellido = "Santana";
+
+echo "No, no. Yo me llamo ". $nombre . " " . $apellido."\n";
