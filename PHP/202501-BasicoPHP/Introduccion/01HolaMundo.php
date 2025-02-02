94741d6c3281365b6d7f6eb94f12424da0ad8afa
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2025-02-02 12:48:48 -0600

    Modificaciones de archivo php

--- PHP/202501-BasicoPHP/Introduccion/01HolaMundo.php
+++ PHP/202501-BasicoPHP/Introduccion/01HolaMundo.php
@@ -1,8 +1,24 @@
 <?php
-echo "Manejo de sintaxis dentro PHP \n";
+/*echo "Manejo de sintaxis dentro PHP \n";
 
 //Manejo de variables
 $nombre = "Carlos";
 $apellido = "Santana";
 
-echo "No, no. Yo me llamo ". $nombre . " " . $apellido."\n";
+echo "No, no. Yo me llamo ". $nombre . " " . $apellido."\n";*/
+
+$personas = [ 
+    "Carlos " => 22,
+    "Mr Michi " => 15,
+    "Juan " => 65
+];
+
+var_dump($personas);
+
+//Como generar comentarios
+// doble diagonal
+# un hast de comentarios
+/**
+ * Comentario extendido
+ */
+echo "\n";
